d6afe58c7ed13f1d5ed1d36c23b7dc592d5c682d
jkelly90 <jlfkelly@gmail.com>
2020-07-05 19:34:40 +0200

    added show posts function

--- script.js
+++ script.js
@@ -16,3 +16,21 @@ async function getPosts() {
     return data;
 }
 
+// Show posts in DOM
+async function showPosts() {
+    const posts = await getPosts();
+
+    posts.forEach(post => {
+        const postEl = document.createElement('div');
+        postEl.classList.add('post');
+        postEl.innerHTML = `
+        <div class="number">${post.id}</div>
+        <div class="post-info">
+          <h2 class="post-title">${post.title}</h2>
+          <p class="post-body">${post.body}</p>
+        </div>
+      `;
+
+        postsContainer.appendChild(postEl);
+    });
+}
